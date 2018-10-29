7c7de544bae1afc0c8a29282e8cd91f0c884f908
gabrieldedominicis <39487395+gabrieldedominicis@users.noreply.github.com>
2018-10-29 00:35:42 -0300

    Views Preguntas y Detalle Nota

---
+++ app/src/main/java/com/ucaece/psicopedia/DetalleNota.kt
@@ -0,0 +1,12 @@
+package com.ucaece.psicopedia
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalleNota : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_nota)
+    }
+}
---
+++ app/src/main/java/com/ucaece/psicopedia/preguntas.kt
@@ -0,0 +1,22 @@
+package com.ucaece.psicopedia
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_preguntas.*
+
+class preguntas : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preguntas)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
